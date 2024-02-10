abcd29af65f58e5b9c7f074fb771b6663ffa48bc
Роман Харитончик <qvelgamer@gmail.com>
2024-02-10 18:37:43 +0300

    1 function left

--- Project1/logic.cpp
+++ Project1/logic.cpp
@@ -18,11 +18,17 @@ bool different_array_numbers(int* array) {
 
 bool happy_number(int number) {
 
-	int currentNum = number;
-	while (currentNum != 1)
+	int numbers[100];
+	for (int i = 0; i < 100; i++)
 	{
+		numbers[i] = 0;
+	}
+
+	for (int i = 0; number != 1; i++)
+	{
+		numbers[i] = number;
 		number = sum_digit_square(number);
-		if (!different_array_numbers)
+		if (!different_array_numbers(numbers))
 		{
 			return false;
 		}
