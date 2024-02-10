92e568782c89c66c16e0c188bab547f4bfe95ea0
Роман Харитончик <qvelgamer@gmail.com>
2024-02-10 19:13:45 +0300

    ended

--- Project1/logic.cpp
+++ Project1/logic.cpp
@@ -11,24 +11,45 @@ int sum_digit_square(int number) {
 	return sum;
 }
 
-bool different_array_numbers(int* array) {
+int get_index_last_natural_num(int* array, int size) {
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] < 1)
+		{
+			return i - 1;
+		}
+	}
+}
+
+bool different_array_numbers(int* array, int size) {
+
+	int lastIndex = get_index_last_natural_num(array, size);
+	for (int i = lastIndex - 1; i >= 0; i--)
+	{
+		if (array[i] == array[lastIndex])
+		{
+			return false;
+		}
+	}
 
 	return true;
 }
 
 bool happy_number(int number) {
 
-	int numbers[100];
-	for (int i = 0; i < 100; i++)
+	const int size = 100;
+	int numbers[size];
+	for (int i = 0; i < size; i++)
 	{
 		numbers[i] = 0;
 	}
 
-	for (int i = 0; number != 1; i++)
+	for (int j = 0; number != 1; j++)
 	{
-		numbers[i] = number;
+		numbers[j] = number;
 		number = sum_digit_square(number);
-		if (!different_array_numbers(numbers))
+		if (!different_array_numbers(numbers, size))
 		{
 			return false;
 		}
