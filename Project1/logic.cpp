66667cfdd0f4428563764eb7a823f43eb2883a19
KharytonchykRaman <157610424+KharytonchykRaman@users.noreply.github.com>
2024-02-12 18:07:02 +0300

    Update logic.cpp
    lol

--- Project1/logic.cpp
+++ Project1/logic.cpp
@@ -11,49 +11,15 @@ int sum_digit_square(int number) {
 	return sum;
 }
 
-int get_index_last_natural_num(int* array, int size) {
-
-	for (int i = 0; i < size; i++)
-	{
-		if (array[i] < 1)
-		{
-			return i - 1;
-		}
-	}
-}
-
-bool different_array_numbers(int* array, int size) {
-
-	int lastIndex = get_index_last_natural_num(array, size);
-	for (int i = lastIndex - 1; i >= 0; i--)
-	{
-		if (array[i] == array[lastIndex])
-		{
-			return false;
-		}
-	}
-
-	return true;
-}
-
 bool happy_number(int number) {
 
-	const int size = 100;
-	int numbers[size];
-	for (int i = 0; i < size; i++)
+	while (number != 1)
 	{
-		numbers[i] = 0;
-	}
-
-	for (int j = 0; number != 1; j++)
-	{
-		numbers[j] = number;
 		number = sum_digit_square(number);
-		if (!different_array_numbers(numbers, size))
+		if (number == 4)
 		{
 			return false;
 		}
 	}
 	return true;
 }
-
